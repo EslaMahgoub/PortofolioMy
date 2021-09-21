c37ba99ef57a3e80e4e271686e0c38e348ab0ea5
exit <exit>
2021-09-21 21:37:30 +0200

    Add Contact and About components

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../ui/organisms/Layout/Layout"
+
+export const AboutPage = (): React.ReactElement => {
+  return (
+    <Layout>
+      <div>
+        This is About Page
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../ui/organisms/Layout/Layout"
+
+
+export const ContactPage = (): React.ReactElement => {
+  return (
+    <Layout>
+      <div>
+        This is Contact Page
+      </div>
+    </Layout>
+  )
+}
