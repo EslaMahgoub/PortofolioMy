a122f1d68a876fdc163b5ca998009a999e71427c
exit <exit>
2021-09-21 21:37:12 +0200

    Add react-router-dom
    Modify index with react-router-dom
    Modify eslint to suppress some errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,12 @@ module.exports = {
         "react",
         "@typescript-eslint"
     ],
-    "rules": {
+    rules: {
+      // suppress errors for missing 'import React' in files
+     "react/react-in-jsx-scope": "off",
+      // allow jsx syntax in js files (for next.js project)
+      'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }], //should add ".ts" if typescript project
+      // suppress error for not knowing props type
+      'react/prop-types': "off"
     }
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import {AboutPage} from './pages/AboutPage/AboutPage';
+import {ContactPage} from './pages/ContactPage/ContactPage';
 
-ReactDOM.render(<div/ >, document.getElementById('root'));
+
+ReactDOM.render(
+  
+  <div > 
+    <BrowserRouter>
+      <Switch>
+        <Route path="/contact" component={ContactPage}/>
+        <Route path="/about" component={AboutPage}/>
+
+        <Route path="/" exact={true}>
+          <Redirect to="/about" exact={true} />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+
+
+  </div>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
