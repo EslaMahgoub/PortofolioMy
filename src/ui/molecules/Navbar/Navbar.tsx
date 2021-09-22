fb06201da0b5e5b62c48cfc434a7ce0cb23c10ae
exit <exit>
2021-09-22 15:21:40 +0200

    Add Icons component
    Add social media links using Icons component and react-env

--- src/ui/molecules/Navbar/Navbar.tsx
+++ src/ui/molecules/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import './Navbar.scss';
 import { useMenu } from 'hooks/useMenu';
 import { history } from 'libs/http/history';
-
+import { Icons } from 'resources/icons/Icons';
+import env from '@beam-australia/react-env';
 
 export const Navbar = (): React.ReactElement => {
   const menu = useMenu();
@@ -14,7 +15,24 @@ export const Navbar = (): React.ReactElement => {
       <div className="navbar">
         <div className="navbar__title">Me</div>
 
-        <div className="navbar__socials">Social Link</div>
+        <div className="navbar__socials">
+          <a href={env("LINKEDIN_URL")} target="_blank" rel="noopener noreferrer" >
+            <Icons type="linkedin"/>
+          </a>
+          <a href={env("FACEBOOK_URL")} target="_blank" rel="noopener noreferrer" >
+            <Icons type="facebook"/>
+          </a>
+          <a href={env("TWITTER_URL")} target="_blank" rel="noopener noreferrer" >
+            <Icons type="twitter"/>
+          </a>
+          <a href={env("INSTAGRAM_URL")} target="_blank" rel="noopener noreferrer" >
+            <Icons type="instagram"/>
+          </a>
+          <a href={env("GITHUB_URL")} target="_blank" rel="noopener noreferrer">
+            <Icons type="github"/>
+          </a>
+
+        </div>
 
       </div>
       <div className="menu"
